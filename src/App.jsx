872a68c4c116a22900e0abdcb25e07eab45ad88c
Ume495 <umekalsoomfatima495@gmail.com>
2024-06-03 15:42:17 +0500

    files added

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import WeatherApp from "./Components/WeatherApp/WeatherApp"
+import "./App.css"
+
+
+
+function App() {
+ 
+
+  return (
+    <div>
+      <WeatherApp/>
+    </div>
+  )
+}
+
+export default App
